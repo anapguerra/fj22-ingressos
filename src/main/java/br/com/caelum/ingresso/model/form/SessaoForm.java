337ef46cf8da97dc09bcc4b7ffc7d22a8178b229
Ana Paula Guerra <anapguerrafernandes@gmail.com>
2017-08-05 09:37:35 -0300

    exercício spring 1

---
+++ src/main/java/br/com/caelum/ingresso/model/form/SessaoForm.java
@@ -0,0 +1,71 @@
+package br.com.caelum.ingresso.model.form;
+
+import java.time.LocalTime;
+
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import br.com.caelum.ingresso.dao.FilmeDao;
+import br.com.caelum.ingresso.dao.SalaDao;
+import br.com.caelum.ingresso.model.Filme;
+import br.com.caelum.ingresso.model.Sala;
+import br.com.caelum.ingresso.model.Sessao;
+
+public class SessaoForm {
+
+	private Integer id;
+	
+	@NotNull
+	private Integer SalaId;
+	@NotNull
+	private Integer FilmeId;
+	@NotNull /*checar formato*/
+	@DateTimeFormat(pattern="HH:mm")
+	private LocalTime horario;
+
+	//getters e setters
+
+	public Integer getSalaId() {
+		return SalaId;
+	}
+
+	public void setSalaId(Integer salaId) {
+		SalaId = salaId;
+	}
+
+	public Integer getFilmeId() {
+		return FilmeId;
+	}
+
+	public void setFilmeId(Integer filmeId) {
+		FilmeId = filmeId;
+	}
+
+	public LocalTime getHorario() {
+		return horario;
+	}
+
+	public void setHorario(LocalTime horario) {
+		this.horario = horario;
+	}
+
+	
+
+	public Sessao toSessao(SalaDao salaDao, FilmeDao filmeDao){
+
+	       Sala sala = SalaDao.findOne(SalaId); //assim o Spring deixa
+	       Filme filme = FilmeDao.findOne(FilmeId); //assim o Spring deixa
+
+	       Sessao sessao = new Sessao(this.horario, filme, sala);
+	       sessao.setId(id);
+
+	       return sessao;
+
+	}
+
+	
+
+	
+	
+}
